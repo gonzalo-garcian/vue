b1c3d0d819645fda8df14b64a26f269a26ee201c
garcian-gonzalo <44837677+gonzalo-garcian@users.noreply.github.com>
2023-02-08 21:21:18 +0100

    migrate: backup

--- js/components/App.js
+++ js/components/App.js
@@ -1,7 +1,49 @@
 import AppButton from "/js/components/AppButton.js";
 
 export default {
-    components: {
-        'app-button': AppButton  
+    template: `
+        <section v-show="inProgress.length">
+            <h2>In progress</h2>
+            <ul>
+                <li v-for="task in inProgress" :key="task.id">
+                    <label>
+                        {{task.name}}
+                        <input type="checkbox" v-model="task.complete">
+                    </label>
+                </li>
+            </ul>
+        </section>
+
+        <section v-show="completed.length">
+            <h2>Completed</h2>
+            <ul>
+                <li v-for="task in completed" :key="task.id">
+                    <label>
+                        {{task.name}}
+                        <input type="checkbox" v-model="task.complete">
+                    </label>
+                </li>
+            </ul>
+        </section>
+    `,
+
+    data () {
+        return {
+            tasks: [
+                {name: "Task 1", complete: false, id: 1},
+                {name: "Task 2", complete: false, id: 2},
+                {name: "Task 3", complete: false, id: 3}
+            ]
+        }
+    },
+
+    computed: {
+        completed() {
+            return this.tasks.filter(t => t.complete);
+        },
+
+        inProgress() {
+            return this.tasks.filter(t => !t.complete)
+        }
     }
 }
