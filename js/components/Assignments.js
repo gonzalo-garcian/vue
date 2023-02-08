b7a8117a7919ab57b91f5692e30eff35b23ed15f
garcian-gonzalo <44837677+gonzalo-garcian@users.noreply.github.com>
2023-02-08 22:54:21 +0100

    added: add new assignment

--- js/components/Assignments.js
+++ js/components/Assignments.js
@@ -5,8 +5,17 @@ export default {
         AssignmentList,
     },
     template: `
+    <section class="space-y-6">
         <assignment-list :assignments="filters.inProgress" title="In Progress"></assignment-list>
         <assignment-list :assignments="filters.completed" title="Completed"></assignment-list>
+
+        <form @submit.prevent="add">
+            <div class="border border-gray-600 text-black">
+                <input v-model="newAssignment" type="text" class="p-2">
+                <button type="submit" class="bg-white p-2 border-l"> Add </button>
+            </div>
+        </form>
+    </section>
     `,
 
     data () {
@@ -15,16 +24,30 @@ export default {
                 {name: "Task 1", complete: false, id: 1},
                 {name: "Task 2", complete: false, id: 2},
                 {name: "Task 3", complete: false, id: 3}
-            ]
+            ],
+
+            newAssignment: "",
         }
     },
 
     computed: {
         filters() {
             return {
-                inProgress: this.assignments.filter(t => t.complete),
-                completed: this.assignments.filter(t => !t.complete)
+                inProgress: this.assignments.filter(t => !t.complete),
+                completed: this.assignments.filter(t => t.complete)
             }
+        }
+    },
+
+    methods: {
+        add() { 
+            this.assignments.push({
+                name: this.newAssignment,
+                complete: false,
+                id: this.assignments.length + 1,
+            });
+
+            this.newAssignment = "";
         }
     }
 }
