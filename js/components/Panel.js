dd691d976fbbf61fb06e54a7216b420018f494e0
garcian-gonzalo <44837677+gonzalo-garcian@users.noreply.github.com>
2023-02-12 19:53:12 +0100

    fixed: panel slots

--- js/components/Panel.js
+++ js/components/Panel.js
@@ -1,13 +1,13 @@
 export default {
     template: `
     <div class="bg-gray-700 p-4 border border-gray-600 rounded-lg">
-        <h2  v-if="$slot.heading" class="font-bold">
+        <h2  v-if="$slots.heading" class="font-bold">
             <slot name="heading" />
         </h2>
 
         <slot/>
 
-        <footer v-if="$slot.footer" class="border-gray-600 border-t mt-4 pt-4 text-sm">
+        <footer v-if="$slots.footer" class="border-gray-600 border-t mt-4 pt-4 text-sm">
             <slot name="footer"/>
         </footer>
     </div>
