55f9cddb7a4c8227c0e32dd7d8f99b8d3570684d
garcian-gonzalo <44837677+gonzalo-garcian@users.noreply.github.com>
2023-02-15 18:04:06 +0100

    added: team view

---
+++ vue-project/src/components/teams/TeamMember.vue
@@ -0,0 +1,20 @@
+<template>
+  <tr>
+    <td class="text-xl font-medium flex items-center gap-x-4 px-6 py-4">
+      <img :src="'https://' + email" alt="" class="rounded-xl" />
+      {{ name }}
+    </td>
+    <td class="text-gray-500 px-6 py-4">{{ email }}</td>
+    <td class="px-6 py-4">
+      <button class="text-green-400 text-xl">{{ status }}</button>
+    </td>
+  </tr>
+</template>
+
+<script setup>
+defineProps({
+  name: String,
+  email: String,
+  status: String,
+});
+</script>
