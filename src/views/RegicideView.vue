3a32ea203c95f39996153ef52c1f48bc66dee812
garcian-gonzalo <44837677+gonzalo-garcian@users.noreply.github.com>
2023-02-18 23:51:31 +0100

    added: take dmg

--- src/views/RegicideView.vue
+++ src/views/RegicideView.vue
@@ -2,14 +2,16 @@
 import CardList from "@/components/cards/CardList.vue";
 import Card from "@/components/cards/Card.vue";
 import regicide from "@/db/regicide.json";
-import { ref, computed, reactive } from "vue";
+import { ref } from "vue";
 
 let castle = ref(regicide.castle);
 let actualEnemy = castle.value.pop();
 let playedCards = ref([]);
-let discardPile = ref([{ number: "test", symbol: "symbol" }]); //ref([]);
+let discardPile = ref([]);
 let deck = ref(regicide.deck);
 let hand = ref(regicide.hand);
+let rcvDmg = false;
+let playing = ref(false);
 
 function shuffleDeck() {
   for (let i = deck.value.length - 1; i > 0; i--) {
@@ -100,17 +102,47 @@ function move() {
   }
 
   doDmg();
+
+  if (actualEnemy.attack > 0 && actualEnemy.hp > 0) {
+    rcvDmg = true;
+  }
+  if (actualEnemy.hp <= 0) {
+    actualEnemy = castle.value.pop();
+  }
 }
 
-function onDrop(evt) {
-  let itemId = evt.dataTransfer.getData("itemID");
-  let cardTransfer = hand.value.splice(
-    hand.value.findIndex((item) => item.number + item.symbol === itemId),
-    1
-  );
-  console.log(cardTransfer);
-  playedCards.value = playedCards.value.concat(cardTransfer);
-  move();
+function onDropPlay(evt) {
+  if (!rcvDmg) {
+    let itemId = evt.dataTransfer.getData("itemID");
+    let cardTransfer = hand.value.splice(
+      hand.value.findIndex((item) => item.number + item.symbol === itemId),
+      1
+    );
+    console.log(cardTransfer);
+    playedCards.value = playedCards.value.concat(cardTransfer);
+    move();
+  }
+}
+
+let actualDiscCards = ref(0);
+function onDropDiscard(evt) {
+  if (rcvDmg) {
+    let itemId = evt.dataTransfer.getData("itemID");
+    let cardTransfer = hand.value.splice(
+      hand.value.findIndex((item) => item.number + item.symbol === itemId),
+      1
+    );
+    console.log(cardTransfer);
+    discardPile.value = discardPile.value.concat(cardTransfer);
+    actualDiscCards.value += parseInt(discardPile.value.at(-1).number);
+    console.log(discardPile.value.at(-1).number);
+    console.log(actualDiscCards.value);
+    console.log(actualEnemy.attack);
+    if (actualDiscCards.value >= actualEnemy.attack) {
+      rcvDmg = false;
+      actualDiscCards.value = 0;
+    }
+  }
 }
 </script>
 
@@ -124,15 +156,23 @@ function onDrop(evt) {
       <Card
         :number="lastCardPlayed().number"
         :symbol="lastCardPlayed().symbol"
-        @drop="onDrop($event)"
+        @drop="onDropPlay($event)"
         @dragover.prevent
         @dragenter.prevent
       />
-      <Card :number="discardPile.length.toString()" />
+      <Card
+        :number="discardPile.length.toString()"
+        @drop="onDropDiscard($event)"
+        @dragover.prevent
+        @dragenter.prevent
+      />
     </div>
     <CardList :cards="hand" />
 
-    <button class="bg-gray-600" @click="draw(7)">Draw!</button>
+    <button v-if="!playing" class="bg-gray-600" @click="draw(8);playing=true">Play!</button>
+  </div>
+  <div v-if="rcvDmg" class="p-10">
+    Damage remaining {{ actualEnemy.attack - actualDiscCards }}
   </div>
 </template>
 
